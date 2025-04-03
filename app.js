fc335b0dec29d16e86599bdee2fadc3e95d55f88
Isayev11 <isayev11onur@gmail.com>
2025-04-03 20:45:55 +0400

    finis

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-console.log("hello")
-alert("hello")
-document.write("hello")
+let ad = prompt("adini daxil et")
+let yaş = prompt("yasini daxil et")
+let sevimliReng = prompt("sevdiyin reng axil et")
+let sevimliFealiyyet = prompt("sevdiyin fealiyyeti daxil et")
+
+console.log(`Salam senin adinn ${ad}. Senin ${yaş} yasin var. Senin en sevdiyin reng ${sevimliReng} rengdir. Senin en sevdiyin fealiyyet ${sevimliFealiyyet}-dir`)
